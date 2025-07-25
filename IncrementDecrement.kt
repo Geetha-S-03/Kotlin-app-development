2fd9629b3f41ae3c1a9d8b768dee77afb4b6813d
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-07-25 21:26:12 +0530

    Create IncrementDecrement.kt

---
+++ IncrementDecrement.kt
@@ -0,0 +1,9 @@
+fun main(){
+    var first:Int=100;
+    first=10;
+    println("$first things")
+    first++;
+    println("Extra $first things")
+    first--;
+    println("Decrement $first things")
+}
