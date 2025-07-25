14b5de09911eb6a17de4fd07847c7c98e7d841e0
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-07-25 21:30:07 +0530

    Create Commands.kt

---
+++ Commands.kt
@@ -0,0 +1,12 @@
+fun main(){
+   // single line command 
+    /*Multiline
+     println("istrue?: "+first)*/
+    //add
+    val a : Int=10;
+    val b : Int=20;
+    /*Adding a &b */
+    val sum : Int=a+b;
+    println("Sum: "+sum)
+    
+}
