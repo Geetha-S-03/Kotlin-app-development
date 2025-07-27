565f276268ad32460dff3ff0a38281904ee9e95e
Geetha-S-03 <168105598+Geetha-S-03@users.noreply.github.com>
2025-07-27 21:45:08 +0530

    Create printMessage.kt

---
+++ printMessage.kt
@@ -0,0 +1,13 @@
+/*Print messages
+Tell your friends what you learned in this pathway.
+
+Can you write a main() function that prints these messages on two separate lines?
+
+Use the val keyword when the value doesn't change. 
+Use the var keyword when the value can change.*/
+
+
+fun main(){
+    println("Use the val keyword when the value doesn't change. ");
+    println("Use the var keyword when the value can change.");
+}
